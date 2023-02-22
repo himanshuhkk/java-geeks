a97ae684bada4fde3116af31deafd05524f6333d
Himanshu Gupta <87234903+himanshuhkk@users.noreply.github.com>
2023-02-22 10:02:22 +0530

    peekneigh

---
+++ peekneigh.java
@@ -0,0 +1,23 @@
+public class peekneigh {
+    public static void main(String[] args){
+        int[] arr = {3,6,45,65,34,76,12,45,78};
+        for(int i=0;i<arr.length;i++){
+            if(i!=0 && i!=arr.length-1){
+                if(arr[i]>=arr[i-1] && arr[i]>=arr[i+1]){
+                    System.out.println(arr[i]);  
+                   
+                }
+          }
+          if(i==0 && arr[i]>=arr[i+1]){
+
+            System.out.println(arr[i]);
+            break;
+        }
+        else if(i==arr.length-1 && arr[i]>=arr[i-1]){
+
+            System.out.println(arr[i]);
+            break;
+        }
+        }
+    }
+}
